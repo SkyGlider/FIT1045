eef3be054db5de9ef2b163b3eaa73a70dbffbf7d
SkyGlider <pyc_andrew@hotmail.com>
2019-03-08 15:42:47 +0800

    Update W1Task4.py

--- W1Task4.py
+++ W1Task4.py
@@ -1,8 +1,11 @@
-import math
-x = int(input("Enter x value : "))
-n = int(input("Enter n value : "))
+#Name : Andrew Pang, Teng Sin Hui
+#Date : 8 Mar 2019
+#Task 2 : Numerical Operations
 
-lga = (math.log(x,math.e))/n
-a = math.exp(lga)
+x = int(input("Enter x value : ")) #Retrieves user input for x value and converts to integer
+n = int(input("Enter n value : ")) #Same for n value
 
-print("The " + str(n) + " root of " + str(x) + " is " + str(a))
+lga = (math.log(x,math.e))/n #calculates log(a)
+a = math.exp(lga) #calculates (a)
+
+print("The " + str(n) + " root of " + str(x) + " is " + str(a)) #prints the answer 
