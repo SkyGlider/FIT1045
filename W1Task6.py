7ca45cf9215a7445acaa57dfd8f436688a72ddff
SkyGlider <pyc_andrew@hotmail.com>
2019-03-08 15:49:08 +0800

    Update W1Task6.py

--- W1Task6.py
+++ W1Task6.py
@@ -1,14 +1,22 @@
-import math
-usrname = input("Enter your name : ")
-usrlength = len(usrname)
-print("The length of your name is " + str(usrlength) + " characters long")
+#Name : Andrew Pang, Teng Sin Hui
+#Date : 8 Mar 2019
+#Task 6
+
+import math #imports math module
+
+usrname = input("Enter your name : ") #get user's name
+
+usrlength = len(usrname) #calculate length of username
+print("The length of your name is " + str(usrlength) + " characters long") #prints the answer above
+
+#counts the number of each vowel in the username
 na = usrname.count("a")
 ne = usrname.count("e")
 ni = usrname.count("i")
 no = usrname.count("o")
 nu = usrname.count("u")
 
-
+#prints the number of each vowels in the username
 print("no. of 'a's :" + str(na))
 print("no. of 'e's :" + str(ne))
 print("no. of 'i's :" + str(ni))
