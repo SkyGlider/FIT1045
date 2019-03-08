0ea0e7bf328a7efb8279da05184241d953dc1c56
SkyGlider <pyc_andrew@hotmail.com>
2019-03-08 15:37:06 +0800

    Add files via upload

---
+++ W1Task2.py
@@ -0,0 +1,20 @@
+#Name : Andrew Pang, Teng Sin Hui
+#Date : 8 Mar 2019
+#Task 2 : Numerical Operations
+
+import math #imports math module
+x=3 #assign 3 to variable x
+y=2 #assign 2 to variable y
+
+print( "x + y = " + str(x + y)) #adds x to y
+print( "x - y = " + str(x - y)) #subtracts y from x
+print( "x * y = " + str(x * y)) #multiplies x and y
+print( "x ** y = " + str(x ** y)) #gives x rasied to the power of y
+print( "x % y = " + str(x % y)) #gives the integer value of x/y
+print( "x / y = " + str(x / y)) #divides x by y
+print( "x // y = " + str(x // y)) #gives the remainder of x/y
+print( "x > y = " + str(x > y)) #checks if x is greater than y
+print( "cos(x/y) = " + str(math.cos(x/y))) #gives cos(x/y)
+
+
+
